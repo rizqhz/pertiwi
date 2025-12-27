1c66ffa0d4f70a574fde8408daf3b2497ee4df8d
rizqhz <rizqi.ghozali@outlook.com>
2025-12-27 15:59:26 +0700

    Add two-point crossover method.

---
+++ genetic/crossover/twopoint.go
@@ -0,0 +1,39 @@
+package crossover
+
+import (
+	. "math/rand/v2"
+
+	. "github.com/rizqhz/pertiwi/genetic/chromosome"
+)
+
+type twopoint struct {
+	rng *Rand
+}
+
+func TwoPoint() *twopoint {
+	return &twopoint{}
+}
+
+func (c *twopoint) Combine(p1, p2 Repr, rate float64) (c1, c2 Repr) {
+	c1 = p1.Clone()
+	c2 = p2.Clone()
+	if n := p1.Len(); c.rng.Float64() < rate {
+		a := c.rng.IntN(n)
+		b := c.rng.IntN(n)
+		for a == b {
+			b = c.rng.IntN(n)
+		}
+		if a > b {
+			a, b = b, a
+		}
+		for i := a; i < b; i++ {
+			c1.Set(i, p2.Get(i))
+			c2.Set(i, p1.Get(i))
+		}
+	}
+	return c1, c2
+}
+
+func (c *twopoint) Random(r *Rand) {
+	c.rng = r
+}
