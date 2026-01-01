5836f25aac09a65f39d792c4acd23ce9dc1fe514
rizqhz <rizqi.ghozali@outlook.com>
2026-01-01 22:37:22 +0700

    Run evolution process concurrently for better performance.

--- genetic/algorithm.go
+++ genetic/algorithm.go
@@ -1,6 +1,9 @@
 package genetic
 
 import (
+	"runtime"
+	"sync"
+
 	. "github.com/rizqhz/pertiwi/genetic/chromosome"
 	"github.com/rizqhz/pertiwi/genetic/strategy"
 )
@@ -14,17 +17,7 @@ func (e *Engine) Evolve(proc chan Repr) Repr {
 		for i := 0; i < e.KeepElitism; i++ {
 			next[i] = best[i].Clone()
 		}
-		for i := e.KeepElitism; i < e.PopulationSize; i += 2 {
-			p1 := e.selector.Select(curr)
-			p2 := e.selector.Select(curr)
-			c1, c2 := e.recombinator.Combine(p1, p2, e.CrossoverRate)
-			e.mutator.Mutate(c1, e.strategy.Rate())
-			e.mutator.Mutate(c2, e.strategy.Rate())
-			next[i] = c1
-			if k := i + 1; k < e.PopulationSize {
-				next[k] = c2
-			}
-		}
+		e.Repopulate(curr, next)
 		curr, next = next, curr
 		e.Compute(curr)
 		best = curr.Best(e.KeepElitism)
@@ -34,8 +27,38 @@ func (e *Engine) Evolve(proc chan Repr) Repr {
 			BestScore: best[0].Score(),
 			Rate:      e.MutationRate,
 		})
-		// proc <- best[0]
+		proc <- best[0].Clone()
 	}
-	// close(proc)
+	close(proc)
 	return best[0]
+}
+
+func (e *Engine) Repopulate(curr, next Set) {
+	var wg sync.WaitGroup
+	jobs := make(chan int, len(curr)/2-e.KeepElitism)
+	worker := func() {
+		defer wg.Done()
+		for i := range jobs {
+			p1 := e.selector.Select(curr)
+			p2 := e.selector.Select(curr)
+			c1, c2 := e.recombinator.Combine(p1, p2, e.CrossoverRate)
+			rate := e.strategy.Rate()
+			e.mutator.Mutate(c1, rate)
+			e.mutator.Mutate(c2, rate)
+			next[i] = c1
+			if k := i + 1; k < e.PopulationSize {
+				next[k] = c2
+			}
+		}
+	}
+	n := runtime.NumCPU()
+	wg.Add(n)
+	for range n {
+		go worker()
+	}
+	for i := e.KeepElitism; i < e.PopulationSize; i += 2 {
+		jobs <- i
+	}
+	close(jobs)
+	wg.Wait()
 }
